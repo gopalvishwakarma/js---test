1742977a43914758db5579c184cc129199c5dadd
GopalVishwakarma <46005154+gopalvishwakarma@users.noreply.github.com>
2024-05-17 13:44:02 +0000

    codemo

--- .devcontainer/testjs/codemo.js
+++ .devcontainer/testjs/codemo.js
@@ -1,10 +1,10 @@
-let score = "true"
+//let score = "true"
 
 
-console.log(typeof score);
+//console.log(typeof score);
 //console.log(typeof (score));
 
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
@@ -14,11 +14,25 @@ let valueInNumber = Number(score)
 // true => 1; false => 0
 
 
-let isLoggedIn = 0
+//let isLoggedIn = 0
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => flse
 // "" => false
 // "hitesh" => true
+
+// *****************************************************operations*******************************************************
+
+
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
